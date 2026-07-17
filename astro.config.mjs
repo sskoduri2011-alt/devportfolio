3e8179083080e92d86cae39ebbe6a8759f004343
Sudeep Koduri <sskoduri2011@gmail.com>
2026-07-16 23:42:21 -0700

    ,

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sskoduri2011-alt.github.io',
-  base: '/devportfolio/',
+  base: '/devportfolio',
   vite: {
     plugins: [tailwindcss()],
   },
